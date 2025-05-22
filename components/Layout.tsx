b3b59ebec484e477dfc0519a1f34fcf160ef8bd5
jonathanpierini <jonathanpierini150783@gmail.com>
2025-05-22 09:50:45 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,1 +1,15 @@
-// Layout component
+import Navbar from './Navbar';
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <Navbar />
+      <main className="p-6">{children}</main>
+    </div>
+  );
+}
